9c9eba979076ee8cd0c6eaa3fb47eb4d94ed8bd2
Edison Wang <ew040607@gmail.com>
2026-08-12 15:06:05 -0400

    feat: env config

---
+++ server/config/env.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"os"
+)
+
+type EnvSettings struct {
+	APIKey string
+}
+
+func MustGetKey(key string) string {
+	val := os.Getenv(key)
+	if val == "" {
+		panic("missing required env var:" + key)
+	}
+	return val
+}
+
+func GetKey(key, fallback string) string {
+	val := os.Getenv(key)
+	if val == "" {
+		return fallback
+	}
+	return val
+}
+
+func NewEnvSettings() *EnvSettings {
+	return &EnvSettings{
+		APIKey: MustGetKey("API_KEY"),
+	}
+}
